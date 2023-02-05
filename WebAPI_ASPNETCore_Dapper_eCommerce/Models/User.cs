3836510b449d38429fd986f86891b7955784d70a
Renato Vilaça <renatovilaca@gmail.com>
2023-02-05 17:19:43 -0300

    Sync User Model with database fields

--- WebAPI_ASPNETCore_Dapper_eCommerce/Models/User.cs
+++ WebAPI_ASPNETCore_Dapper_eCommerce/Models/User.cs
@@ -4,6 +4,12 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }   
-        public string Email { get; set; }   
+        public string Email { get; set; }  
+        public string Gender { get; set; }
+        public string RG { get; set; }
+        public string CPF { get; set; }
+        public string Mother { get; set; }
+        public string Status { get; set; }
+        public DateTimeOffset CreatedAt { get; set; }
     }
 }
